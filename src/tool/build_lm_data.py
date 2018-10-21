f5fa7124a8780beff5c52aa9062ce15a7d08fef5
Ed Lui <rewq142857@hotmail.com>
2018-10-21 14:06:17 +0800

    Update build_lm_data.py

--- src/tool/build_lm_data.py
+++ src/tool/build_lm_data.py
@@ -1,5 +1,5 @@
 import sys
-f=open(sys.argv[1],'r')
+f=open(sys.argv[1],'r')  #data/${main_data:[yelp,amazon,imagecaption]}/sentiment.train.${i:[0,1]}
 fw=open(sys.argv[2]+'.lm','w')
 for line in f:
 	fw.write('1\t'+line.strip()+'\t1\t1\n')
